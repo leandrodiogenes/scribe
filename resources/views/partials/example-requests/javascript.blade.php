f0c483d60eb6df2f67e4841caa63ec6144052352
Leandro <ldstudio.com@gmail.com>
2020-09-03 10:24:26 -0300

    Melhorado erro do scribe em formrequests com problemas

--- resources/views/partials/example-requests/javascript.blade.php
+++ resources/views/partials/example-requests/javascript.blade.php
@@ -1,41 +1,82 @@
 ```javascript
-const url = new URL(
-    "{{ rtrim($baseUrl, '/') }}/{{ ltrim($route['boundUri'], '/') }}"
-);
+
+//------------------------------------
+//------------ ApiRequest ------------
+//------------------------------------
+
+@if(count($route['cleanQueryParameters']))
+    let params = {!! \Knuckles\Scribe\Tools\WritingUtils::printQueryParamsAsKeyValue($route['cleanQueryParameters'], "\"", ":", 4, "{}") !!};
+@endif
+@if(count($route['cleanBodyParameters']))
+    let params = {!! json_encode($route['cleanBodyParameters'], JSON_PRETTY_PRINT) !!}
+@endif
+
+new ApiRequest({
+url: "{{ rtrim($baseUrl, '/') }}/{{ ltrim($route['boundUri'], '/') }}",
+method: "{{$route['methods'][0]}}",
+@if($route['methods'][0] == "PUT")
+    raw: true,
+@endif
+success: function(response){ }
+})
+@foreach($route['fileParameters'] as $parameter => $file)
+    .addParameter('{!! $parameter !!}', document.querySelector('input[name="{!! $parameter !!}"]').files[0]);
+@endforeach
+@if(count($route['cleanQueryParameters']) or count($route['cleanBodyParameters']))
+    .addParameters(params)
+@endif
+.execute();
+
+
+
+
+
+
+
+
+//-----------------------------------------
+//------------ JAVASCRIPT puro ------------
+//-----------------------------------------
+
+const url = new URL("{{ rtrim($baseUrl, '/') }}/{{ ltrim($route['boundUri'], '/') }}");
 @if(count($route['cleanQueryParameters']))
 
-let params = {!! \Knuckles\Scribe\Tools\WritingUtils::printQueryParamsAsKeyValue($route['cleanQueryParameters'], "\"", ":", 4, "{}") !!};
-Object.keys(params)
-    .forEach(key => url.searchParams.append(key, params[key]));
+    Object.keys(params).forEach(key => url.searchParams.append(key, params[key]));
 @endif
-
 @if(!empty($route['headers']))
-let headers = {
-@foreach($route['headers'] as $header => $value)
-    "{{$header}}": "{{$value}}",
-@endforeach
-@if(!array_key_exists('Accept', $route['headers']))
-    "Accept": "application/json",
-@endif
-};
-@endif
+    let headers = {
+    @foreach($route['headers'] as $header => $value)
+        "{{$header}}": "{{$value}}",
+    @endforeach
+    @if(!array_key_exists('Accept', $route['headers']))
+        "Accept": "application/json",
+    @endif
+    };
 
+@endif
 @if(count($route['fileParameters']))
-const body = new FormData();
-@foreach($route['cleanBodyParameters'] as $parameter => $value)
-@foreach( \Knuckles\Scribe\Tools\WritingUtils::getParameterNamesAndValuesForFormData($parameter,$value) as $key => $actualValue)
-body.append('{!! $key !!}', '{!! $actualValue !!}');
-@endforeach
-@endforeach
-@foreach($route['fileParameters'] as $parameter => $file)
-body.append('{!! $parameter !!}', document.querySelector('input[name="{!! $parameter !!}"]').files[0]);
-@endforeach
+    const body = new FormData();
+    @foreach($route['cleanBodyParameters'] as $parameter => $value)
+        @foreach( \Knuckles\Scribe\Tools\WritingUtils::getParameterNamesAndValuesForFormData($parameter,$value) as $key => $actualValue)
+            @php
+                try {
+                    echo "body.append('$key', '$actualValue')";
+                } catch (\Exception $e) {
+                    throw new Exception("Há um problema com o parametro {$key} na rota ".$route['methods'][0]." ".$route['uri']);
+                }
+            @endphp
+
+        @endforeach
+    @endforeach
+    @foreach($route['fileParameters'] as $parameter => $file)
+        body.append('{!! $parameter !!}', document.querySelector('input[name="{!! $parameter !!}"]').files[0]);
+    @endforeach
 @elseif(count($route['cleanBodyParameters']))
-let body = {!! json_encode($route['cleanBodyParameters'], JSON_PRETTY_PRINT) !!}
+    let body = {!! json_encode($route['cleanBodyParameters'], JSON_PRETTY_PRINT) !!}
 @endif
 
 fetch(url, {
-    method: "{{$route['methods'][0]}}",
+method: "{{$route['methods'][0]}}",
 @if(count($route['headers']))
     headers: headers,
 @endif
@@ -43,6 +84,8 @@ fetch(url, {
     body: body
 @endif
 })
-    .then(response => response.json())
-    .then(json => console.log(json));
+.then(response => response.json())
+.then(json => console.log(json));
+
+
 ```
